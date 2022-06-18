90d0a8758bfb56ae87c3482a7a6261c622cfff06
Godswill Dennis <gdee.arch@gmail.com>
2022-06-18 12:47:47 +0100

    updated

--- i4g012100saz/blog/models.py
+++ i4g012100saz/blog/models.py
@@ -1,3 +1,11 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200 )
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+    
